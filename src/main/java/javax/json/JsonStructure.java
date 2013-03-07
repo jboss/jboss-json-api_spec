8cdf5895bd0cbafb56b835abcd7525e8cb52fc9b
James Perkins <jperkins@redhat.com>
2013-03-07 14:46:56 -0800

    Add API source.

---
+++ src/main/java/javax/json/JsonStructure.java
@@ -0,0 +1,10 @@
+package javax.json;
+
+/**
+ * Super type for the two structured types in JSON ({@link JsonObject object}s
+ * and {@link JsonArray array}s).
+ *
+ * @author Jitendra Kotamraju
+ */
+public interface JsonStructure extends JsonValue {
+}
